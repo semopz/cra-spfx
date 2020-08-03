f1eb1264fd5616f48263d9ae0bd437055aef3c1c
Pavel Aralanov <paralanov@cclcompliance.co.uk>
2020-08-03 20:21:15 +0100

    added apps

---
+++ spfx/wp-2019/src/webparts/webpart2019Test/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IWebpart2019TestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'Webpart2019TestWebPartStrings' {
+  const strings: IWebpart2019TestWebPartStrings;
+  export = strings;
+}
